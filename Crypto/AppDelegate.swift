0657cd83dd8e2774a3dfc78cf0ed83062fa1244d
chhan-sophearith <chhansophearithit@gmail.com>
2024-07-17 11:23:11 +0700

    16 build

---
+++ Crypto/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  InfoWebiOS
+//
+//  Created by Chhan Sophearith on 7/12/23.
+//
+
+import Foundation
+import UIKit
+import IQKeyboardManagerSwift
+import FirebaseCore
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        IQKeyboardManager.shared.enable = true
+        FirebaseApp.configure()
+        return true
+    }
+    
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    }
+}
